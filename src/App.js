8398448b3cb09f95ae9ce7e45c5ba3bb603f238d
rody josue <rody.josue.cc.c@gmail.com>
2022-04-07 22:42:04 -0500

    installed of fontwesome

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Nosotros from './components/Nosotros';
+import Reviews from './components/Reviews';
 
 function App() {
   return (
     <>
-
+    <Reviews/>
+    <Nosotros/>
     </>
   );
 }
