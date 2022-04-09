44cc1072ba3239f88049ef43649314fc4558bb2a
Victoria Lavarello <97409272+CodexVex@users.noreply.github.com>
2022-04-09 13:32:51 -0500

    Update PageNotFound.js

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -2,17 +2,22 @@ import React from 'react';
 import '../styles/css/PageNotFound.css';
 
 const PageNotFound = () => {
+   <body class="body">
     <div>
-        <img class="background"
-             src='https://s3.amazonaws.com/shecodesio-production/uploads/files/000/031/392/original/91DE8079-3A3D-4D02-A317-BD18903CE4CA.png?1649495448'
-             alt="espacio-constelacion"/>
-        <img class="title"
-             src='https://s3.amazonaws.com/shecodesio-production/uploads/files/000/031/394/original/F1D71005-F5A8-4BAC-B0C0-CA5DF208AFA5.png?1649495464'
-             alt="error 404"/>
-        <img class="alpaca"
-             src='https://s3.amazonaws.com/shecodesio-production/uploads/files/000/031/393/original/C5BDC664-483D-4885-BD3B-37634E12603D.png?1649495458'
-             alt="llama espacial artesaner"/>
+      <img
+        class="title"
+        src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/031/435/original/49306444-1F68-4BC4-A543-09E043E10949.png?1649528823"
+        alt="error 404"
+        id="elements"
+      />
+      <img
+        class="alpaca"
+        src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/031/436/original/689AF8F5-23A8-40B0-B25D-1ED5577C5C85.png?1649528832"
+        alt="llama espacial artesaner"
+        id="elements"
+      />
     </div>
+  </body>
 }
 
 export default PageNotFound;
