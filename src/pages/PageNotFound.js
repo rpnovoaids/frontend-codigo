6cf688ba7df32a3af26183e894c3510308f10965
Victoria Lavarello <97409272+CodexVex@users.noreply.github.com>
2022-04-09 04:20:33 -0500

    order-fixing

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -3,8 +3,8 @@ import React from 'react';
 const PageNotFound= () =>{
 	<div>
 		<img class="background"src='https://s3.amazonaws.com/shecodesio-production/uploads/files/000/031/392/original/91DE8079-3A3D-4D02-A317-BD18903CE4CA.png?1649495448' alt="espacio-constelacion"/>
-		<img class="alpaca"src='https://s3.amazonaws.com/shecodesio-production/uploads/files/000/031/393/original/C5BDC664-483D-4885-BD3B-37634E12603D.png?1649495458' alt="llama espacial artesaner">
-	  	<img class="title" src='https://s3.amazonaws.com/shecodesio-production/uploads/files/000/031/394/original/F1D71005-F5A8-4BAC-B0C0-CA5DF208AFA5.png?1649495464' alt="error 404">   
+	  	<img class="title" src='https://s3.amazonaws.com/shecodesio-production/uploads/files/000/031/394/original/F1D71005-F5A8-4BAC-B0C0-CA5DF208AFA5.png?1649495464' alt="error 404">
+			<img class="alpaca"src='https://s3.amazonaws.com/shecodesio-production/uploads/files/000/031/393/original/C5BDC664-483D-4885-BD3B-37634E12603D.png?1649495458' alt="llama espacial artesaner">
    </div>
 }
 
